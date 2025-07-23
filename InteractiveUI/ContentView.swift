458d3a0ef8f92e04afadf44bfc92f374bfb32574
Krisha <akrisha259@gmail.com>
2025-07-23 14:11:47 -0700

    add button functionality, edited text to display name input

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name: String = ""
+    @State private var name = ""
     @State private var textTitle = "What is your name?"
     
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
             TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.pink, width: 1)
             Button("submit name") {
-
+                textTitle = "Welcome, " + name + "!"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
