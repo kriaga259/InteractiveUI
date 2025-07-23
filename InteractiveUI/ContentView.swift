7c52fe785b5f536eeadea4ce0821f8a953a6f523
Krisha <akrisha259@gmail.com>
2025-07-23 14:29:42 -0700

    added mood button and alert

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var name = ""
     @State private var textTitle = "What is your name?"
     
+    
+    @State private var mood = ""
+    @State private var presentAlert = false // binding val
+    
     var body: some View {
         VStack {
             Text(textTitle)
@@ -20,13 +24,34 @@ struct ContentView: View {
                 .font(.title)
                 .border(Color.pink, width: 1)
             Button("submit name") {
-                textTitle = "Welcome, " + name + "!"
+                textTitle = "Welcome, \(name)!"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
+            
+            Spacer()
+            
+            Text("How are you feeling today?")
+                .font(.title)
+            TextField("Type your mood here...", text: $mood)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.pink, width: 1)
+//            Text(presentAlert ? "Presenting": "Dismissed")
+            //what does the above line do??
+                
+            
+            Button("submit mood") {
+                presentAlert = true
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
 
         }
+        .alert("moody .-.", isPresented: $presentAlert, actions: {})
         .padding()
     }
 }
